9bccad615e9cff607ba7b6fc383ec5c13c9564fb
Beadd <flyships@qq.com>
2025-11-06 21:58:54 +0800

    r

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,56 @@
-// @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import antfu from "@antfu/eslint-config";
+// import css from "@eslint/css";
+// import { tailwindSyntax } from "@eslint/css/syntax";
+import eslintPluginReadableTailwind from "eslint-plugin-readable-tailwind";
+import eslintParserVue from "vue-eslint-parser";
 
-export default withNuxt(
-  // Your custom configs here
-)
+export default antfu(
+  {
+    stylistic: {
+      indent: 2, // 4, or 'tab'
+      quotes: "double", // or 'single'
+      semi: true,
+    },
+  },
+  {
+    rules: {
+      "style/brace-style": ["error", "1tbs"],
+    },
+  },
+  // {
+  //   files: ["**/*.css"],
+  //   plugins: {
+  //     css,
+  //   },
+  //   language: "css/css",
+  //   languageOptions: {
+  //     customSyntax: tailwindSyntax,
+  //   },
+  //   rules: {
+  //     "css/no-empty-blocks": "error",
+  //   },
+  // },
+  {
+    files: ["**/*.vue"],
+    languageOptions: {
+      parser: eslintParserVue,
+    },
+  },
+  {
+    plugins: {
+      "readable-tailwind": eslintPluginReadableTailwind,
+    },
+    rules: {
+      // enable all recommended rules to warn
+      ...eslintPluginReadableTailwind.configs.warning.rules,
+      // enable all recommended rules to error
+      ...eslintPluginReadableTailwind.configs.error.rules,
+
+      // or configure rules individually
+      "readable-tailwind/multiline": ["warn", { printWidth: 80 }],
+    },
+  },
+  {
+    ignores: ["database.types.ts", "android", "ios", "public"],
+  },
+);
