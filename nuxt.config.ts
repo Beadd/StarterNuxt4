c5b1d7d48fb84bc1ccaaebf9e9b63408b15a2776
Beadd <flyships@qq.com>
2025-11-06 21:38:19 +0800

    r

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@pinia/nuxt',
+    'pinia-plugin-persistedstate',
+    '@nuxtjs/i18n',
+    '@nuxt/eslint',
+    '@primevue/nuxt-module'
+  ]
 })
