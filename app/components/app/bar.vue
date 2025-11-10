f88f4c4ce6ffeb5ec661cac7dc0a4ba256fea214
Beadd <flyships@qq.com>
2025-11-10 17:54:51 +0800

    Update bar.vue

--- app/components/app/bar.vue
+++ app/components/app/bar.vue
@@ -10,7 +10,7 @@ const profiles = useprofilestore();
 <template>
   <div class="sticky top-0 z-21 w-full backdrop-blur-xl">
     <div class="h-st w-full" />
-    <div class="flex h-16 w-full border-b border-neutral">
+    <div class="border-neutral flex h-16 w-full border-b">
       <div class="flex h-full w-full items-center justify-between gap-4 px-4">
         <div class="flex items-center gap-2">
           <AppMenu v-if="show" class="p-0!" @click="useRouter().push('/mine')">
@@ -23,7 +23,7 @@ const profiles = useprofilestore();
             />
           </AppMenu>
           <AppMenu v-else @click="useRouter().back()">
-            <i class="bx bx-chevron-left-circle text-3xl text-primary" />
+            <i class="bx bx-chevron-left-circle text-primary text-3xl" />
           </AppMenu>
           <ProfileName
             class="
