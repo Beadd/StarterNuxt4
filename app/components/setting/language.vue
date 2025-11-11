7734220794c7f70ba0fabb8a7b644b51460ec0bb
Beadd <flyships@qq.com>
2025-11-11 22:52:19 +0800

    Create language.vue

---
+++ app/components/setting/language.vue
@@ -0,0 +1,18 @@
+<script setup>
+const { locales, setLocale, locale } = useI18n();
+
+function change(event) {
+  setLocale(event.target.value);
+}
+</script>
+
+<template>
+  <div class="flex flex-col gap-4 p-4">
+    {{ $t("Language") }}
+    <app-select
+      v-model="locale"
+      :options="locales.map((l) => ({ label: l.name, value: l.code }))"
+      @change="change"
+    />
+  </div>
+</template>
