b62e0450ab3506876823cb5e9289b4cd256569cb
Beadd <flyships@qq.com>
2025-11-12 20:27:22 +0800

    r

--- app/components/notification/avatar.vue
+++ app/components/notification/avatar.vue
@@ -9,30 +9,19 @@ defineProps<{
   <div class="avatar placeholder cursor-pointer select-none">
     <div
       v-if="severity === 'info'"
-      class="
-        avatar bg-info text-neutral-content ring-base-100 w-12 rounded-full ring
-      "
+      class="avatar bg-info text-neutral-content ring-base-100 w-12 rounded-full ring"
     />
     <div
       v-else-if="severity === 'success'"
-      class="
-        avatar bg-success text-neutral-content ring-base-100 w-12 rounded-full
-        ring
-      "
+      class="avatar bg-success text-neutral-content ring-base-100 w-12 rounded-full ring"
     />
     <div
       v-else-if="severity === 'warning'"
-      class="
-        avatar bg-warning text-neutral-content ring-base-100 w-12 rounded-full
-        ring
-      "
+      class="avatar bg-warning text-neutral-content ring-base-100 w-12 rounded-full ring"
     />
     <div
       v-else-if="severity === 'error'"
-      class="
-        avatar bg-error text-neutral-content ring-base-100 w-12 rounded-full
-        ring
-      "
+      class="avatar bg-error text-neutral-content ring-base-100 w-12 rounded-full ring"
     />
     <ProfileAvatar
       v-else
