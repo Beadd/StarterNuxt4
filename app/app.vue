4eff753cdedb2e13874706ba7fd9b848611f01cc
Beadd <flyships@qq.com>
2025-11-13 21:36:45 +0800

    r

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,26 @@
+<script setup lang="ts">
+import { displayName } from "../package.json";
+
+useHead({
+  title: displayName,
+  htmlAttrs: {
+    class: "select-none",
+  },
+  meta: [
+    {
+      name: "viewport",
+      content: "width=device-width, initial-scale=1, maximum-scale=1",
+    },
+  ],
+});
+</script>
+
 <template>
-  <AppNew name="test" path="/test" />
+  <NuxtLoadingIndicator color="#1d9bf0" :throttle="0" class="top-st!" />
+  <NuxtLayout>
+    <NuxtPage />
+    <PluginClipboard />
+  </NuxtLayout>
+  <AppLoading />
+  <AppToast />
 </template>
---
+++ app/components/plugin/clipboard.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const plugins = usepluginstore();
+
+async function confirm() {
+  useRouter().push(`${plugins.clipboard.link}`);
+  plugins.clipboard.ignore = plugins.clipboard.link;
+}
+</script>
+
+<template>
+  <AppDialog
+    :bus="plugins.clipboard.bus"
+    :confirm="confirm"
+    :cancel="async () => { plugins.clipboard.ignore = plugins.clipboard.link }"
+    :content="$t('A share link was detected in the clipboard. Do you want to open it?')"
+  />
+</template>
